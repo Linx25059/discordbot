da81a78fdcd3bf637abda534f5f76c39a14d1ef7
linx <linx25059@gmail.com>
2026-05-15 03:28:51 +0800

    feat(jerkcounter): 擴充搞笑語錄與迷因 GIF 圖庫，大幅增加觸發時的隨機樂趣

--- cogs/jerk_counter.py
+++ cogs/jerk_counter.py
@@ -17,8 +17,8 @@ class JerkCounter(commands.Cog):
         if message.author.bot:
             return
 
-        # 設定觸發關鍵字
-        keywords = ["尻", "打手槍", "清槍", "打管", "擼","導管","撸管","撸","撸一撸","打飞机","打飛機","cum","jerk","masturbate","wank","fap","self pleasure","手淫","自慰"]
+        # 設定觸發關鍵字 (在這裡加入新的詞彙)
+        keywords = ["尻", "打手槍", "清槍", "打管", "擼","導管","撸管","撸","撸一撸","打飞机","打飛機","cum","jerk","masturbate","wank","fap","self pleasure","手淫","自慰", "打膠", "尻尻"]
         
         if any(k in message.content for k in keywords):
             async with self.bot.db.db.execute('SELECT count FROM jerk_counts WHERE user_id = ?', (message.author.id,)) as cursor:
@@ -56,11 +56,36 @@ class JerkCounter(commands.Cog):
                 "📉 警告：{mention} 的生命值正在急速下降... 目前清槍次數已達 **{count}** 次！",
                 "🏆 掌聲鼓勵！{mention} 達成了第 **{count}** 次清槍成就！難道這就是傳說中的尻神？",
                 "🧘‍♂️ 施主，色即是空，空即是色。{mention}，你已經累積 **{count}** 次了...",
-                "🙈 我什麼都沒看到... 但系統無情地記錄下 {mention} 發動了第 **{count}** 次傳統手藝。"
+                "🙈 我什麼都沒看到... 但系統無情地記錄下 {mention} 發動了第 **{count}** 次傳統手藝。",
+                "🚨 偵測到異常的雙手運動頻率！{mention} 的累積次數已經來到 **{count}** 次！",
+                "🛑 快停下來！{mention}，你的鍵盤和滑鼠在抗議了！這是第 **{count}** 次了！",
+                "🚑 誰來幫 {mention} 叫輛救護車，這已經是他第 **{count}** 次施展奧義了！",
+                "📜 歷史的卷軸又添上了一筆：{mention} 完成了第 **{count}** 次的秘密儀式。",
+                "🔋 魔法值即將耗盡... {mention} 的施法次數：**{count}** 次。",
+                "🌟 什麼？你說這已經是 {mention} 的第 **{count}** 次了？真是太無情了！"
             ]
+            
+            # 新增搞笑 GIF 圖片庫 (可以隨意替換或新增自己的網址)
+            funny_gifs = [
+                "https://media.tenor.com/7D2_hKzY-VwAAAAC/bonk-doge.gif", # 柴犬敲頭 Bonk
+                "https://media.tenor.com/YhWf3H80Xk8AAAAC/fbi-fbi-open-up.gif", # FBI 開門
+                "https://media.tenor.com/bZeW7hXv_LMAAAAC/stop-it-get-some-help.gif", # 麥可喬丹勸世
+                "https://media.tenor.com/JkKj-kR5DfkAAAAC/sus-rock.gif", # 巨石強森懷疑
+                "https://media.tenor.com/6U-0q9u1OIgAAAAC/caught-in-4k.gif", # Caught in 4K 抓包
+                "https://media.tenor.com/Z4WlJ-uK7vAAAAAC/cat-smack.gif", # 貓咪敲頭
+                "https://media.tenor.com/lx2Fl-n8x1YAAAAC/facepalm-picard.gif", # 艦長無奈捂臉
+                "https://media.tenor.com/81oE4gE1D6MAAAAC/stop-it.gif", # 停止啦
+                "https://media.tenor.com/kS9lS-6zFpQAAAAC/stare-look.gif", # 死亡凝視
+                "https://media.tenor.com/5tLdG_6E83cAAAAC/shocked-face.gif", # 驚呆了
+                "https://media.tenor.com/GIf9gZ8oT_kAAAAC/gordon-ramsay-stop.gif" # 地獄廚神大喊停
+            ]
+
             reply_text = random.choice(funny_replies).format(mention=message.author.mention, count=new_count)
+            embed = discord.Embed(color=discord.Color.red())
+            embed.set_image(url=random.choice(funny_gifs))
+            
             try:
-                await message.reply(reply_text, mention_author=False)
+                await message.reply(reply_text, embed=embed, mention_author=False)
             except:
                 pass
 
